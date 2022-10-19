1fd83dc56a84d143e20443bc230675de60452092
Saurav Jaiswal <78429575+liamRogue@users.noreply.github.com>
2022-10-19 13:13:16 +0545

    insertion at end of linked list

---
+++ linked list/insertionAtEnd.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node *next;
+};
+struct node *head,*newnode,*temp;
+void createLL()
+{
+    int choice,count=0;
+    head=0;
+    while(choice)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        printf("enter data:");
+        scanf("%d",&newnode->data);
+        newnode->next=0;
+        if(head==0)
+        {
+            head=temp=newnode;
+        }
+        else{
+            temp->next=newnode;
+            temp=newnode;
+        }
+        printf("do you want to continue(0/1)?");
+        scanf("%d",&choice);
+    }
+    temp=head;
+    while(temp!=0)
+    {
+        printf("%d\t",temp->data);
+        temp=temp->next;
+        count++;
+    }
+    printf("\ntotal no. of nodes created: %d",count);
+    getch();
+}
+void main()
+{
+    int count=0;
+    // count=createLL();
+    createLL();
+    newnode=(struct node*)malloc(sizeof(struct node));
+    printf("enter data:");
+    scanf("%d",&newnode->data);    
+    newnode->next=0;
+    temp=head;
+    while(temp->next!=0)
+    {
+        temp=temp->next;
+    }
+    temp->next=newnode;
+    temp=head;
+    while(temp!=0)
+    {
+        printf("%d\t",temp->data);
+        temp=temp->next;
+        count++;
+    }
+    printf("\ntotal no. of nodes created: %d",count);
+
+}
